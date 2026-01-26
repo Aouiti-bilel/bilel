0baf1e9b4bf9db5d7b1f133f385c8db5654a10fe
Aouiti-bilel <khelifa.bilel1@outlook.fr>
2026-01-26 18:45:28 +0100

    Wip

--- app/admin/blog/page.tsx
+++ app/admin/blog/page.tsx
@@ -1,4 +1,5 @@
 import prisma from "@/lib/prisma";
+
 import Main from "./Main";
 
 
@@ -8,4 +9,39 @@ type Props = {
         page?: string;
     };
 };
-git 
+const PAGE_SIZE = 20;
+
+export default async function BlogListPage(props: Props) {
+    const searchParams = await props?.searchParams
+
+    const page = Number(searchParams.page || 1);
+    const q = searchParams.q || "";
+
+
+    const where: any = {
+        AND: [
+            q
+                ? {
+                    OR: [
+                        { title: { contains: q, mode: "insensitive" } },
+                        { description: { contains: q, mode: "insensitive" } },
+                    ],
+                }
+                : {},
+        ],
+    }
+    const [posts, total] = await Promise.all([
+        prisma.blog.findMany({
+            where: where,
+            orderBy: { createdAt: "desc" },
+            take: PAGE_SIZE,
+            skip: (page - 1) * PAGE_SIZE,
+        }),
+        prisma.blog.count({ where }),
+    ]);
+
+    const totalPages = Math.ceil(total / PAGE_SIZE);
+    return (
+        <Main posts={posts} totalPages={totalPages} />
+    );
+}
